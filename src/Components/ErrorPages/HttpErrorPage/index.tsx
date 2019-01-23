e8f588394f719dd574541a3b05bd3e554132565d
Sagar Jain <sagar@zestmoney.in>
2019-01-23 13:16:09 +0530

    added route guard for http service error and also added rest api client service

--- src/Components/ErrorPages/HttpErrorPage/index.tsx
+++ src/Components/ErrorPages/HttpErrorPage/index.tsx
@@ -10,23 +10,19 @@ export const HttpErrorPage = (props: Props) => {
 
     const error = {
         errorCode: props.httpErrorCode,
-        shortErrorMessage: 'Something went wrong, please try again later',
+        shortErrorMessage: 'Oops...',
         longErrorMessage: 'Something went wrong, please try again later.'
     };
     
     if (props.httpErrorCode === 401) {
-        error.shortErrorMessage = '401 - Unauthorized';
         error.longErrorMessage = 'You are not authorized to view this page. Please login to continue.';
     } else if (props.httpErrorCode === 500) {
         error.shortErrorMessage = '500 - Internal server error';
-        error.longErrorMessage = 'Something went wrong, please try again later.';
     }
 
     return (
         <div className="error-container">
-            <div className="error-icon">
-                <div className="error-icon-eyes"></div>
-            </div>
+            <div className="error-icon"></div>
             <div className="error-note">
                 <p className="title-note">
                     {error.shortErrorMessage}
